3d739f3adbd5fd4080eeb10db144881d5c5522af
AndreeaTM <andreeateodora.marinica@yahoo.com>
2019-10-11 20:04:16 +0300

    Changes

--- Conference/Conference/wwwroot/js/site.js
+++ Conference/Conference/wwwroot/js/site.js
@@ -2,5 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
+var countDownDate = new Date("Feb 29, 2020").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.getElementById("demo").innerHTML = days + " " + "days to the event";
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
